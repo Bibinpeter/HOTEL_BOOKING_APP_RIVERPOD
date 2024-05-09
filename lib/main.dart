692e97857fe4477f6103d4f2e08d0879a10b5b6d
bibin peter <bibinpeter2018@gmail.com>
2024-05-09 15:27:44 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hotel/firebase/firebase_options.dart';
+import 'package:hotel/screen/splashscreen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Add this line
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,69 +19,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(scaffoldBackgroundColor: const Color(0xfff6f6f6),),
+      debugShowCheckedModeBanner: false,
+      home:SplashScreen()
+      
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    
-    return Scaffold(
-      appBar: AppBar(
-         
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      
-        title: Text(widget.title),
-      ),
-      body: Center(
-       
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
-    );
-  }
-}
+ 
---
+++ lib/screen/splashscreen.dart
@@ -0,0 +1,72 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hotel/screen/auth/login.dart';
+import 'package:hotel/screen/home/home.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    const colorizeColors = [
+      Color.fromARGB(255, 246, 244, 244),
+      Color.fromARGB(255, 50, 51, 51),
+      Color.fromARGB(255, 43, 43, 42),
+      Color.fromARGB(255, 75, 75, 75),
+    ];
+    return AnimatedSplashScreen(
+      splash: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 80),
+              child: LottieBuilder.asset(
+                'assets/animation/animation.json',
+                fit: BoxFit.fill,
+              ),
+            ),
+            const SizedBox(
+              height: 60,
+            ),
+            AnimatedTextKit(
+              animatedTexts: [
+                ColorizeAnimatedText(
+                  'GRAB YOUR OWN',
+                  textStyle: TextStyle(
+                    fontSize: 24,
+                    fontFamily: GoogleFonts.poppins().fontFamily,
+                  ),
+                  colors: colorizeColors,
+                ),
+                ColorizeAnimatedText(
+                  'SPACE ANYWHERE',
+                  textStyle: TextStyle(
+                    fontSize: 24,
+                    fontFamily: GoogleFonts.poppins().fontFamily,
+                  ),
+                  colors: colorizeColors,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Colors.black,
+      nextScreen: FirebaseAuth.instance.currentUser == null
+          ? const LoginPage()
+          : Homepage(),
+      splashIconSize: 500,
+      duration: 4000,
+      splashTransition: SplashTransition.fadeTransition,
+    );
+  }
+}
